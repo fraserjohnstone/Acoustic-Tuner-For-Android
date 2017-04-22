e521e5b3d3d3bce2f69effa6b2d6c424ac1e39bb
Fraser <fraserjohnstone12345@hotmail.com>
2017-04-22 18:29:08 +0100

    Code aesthetics

--- core/src/com/fraserjohnstone/tuner/Tuner.java
+++ core/src/com/fraserjohnstone/tuner/Tuner.java
@@ -16,86 +16,86 @@ import com.fraserjohnstone.tuner.utils.ActionResolver;
  * the screen to an instance of {@link com.fraserjohnstone.tuner.screens.SplashScreen} to be displayed
  * while the application assets load into {@link #mAssetManager}.
  *
+ * @author Fraser Johnstone
  * @version 1.01 - 22.04.2017
- * @author  Fraser Johnstone
  */
 public class Tuner extends Game {
 
-	public int mScreenHeightPixels;
-	public int mScreenWidthPixels;
+    public int mScreenHeightPixels;
+    public int mScreenWidthPixels;
 
-	public SpriteBatch mBatch;
-	public AssetManager mAssetManager;
+    public SpriteBatch mBatch;
+    public AssetManager mAssetManager;
 
-	public ActionResolver mActionResolver;
+    public ActionResolver mActionResolver;
 
-	/**
-	 * Class constructor.
-	 * <p>
-	 * Sets up all the objects required throughout the Application and displays the splash screen.
-	 */
-	public Tuner(ActionResolver _actionResolver){
-		mActionResolver = _actionResolver;
-	}
+    /**
+     * Class constructor.
+     * <p>
+     * Sets up all the objects required throughout the Application and displays the splash screen.
+     */
+    public Tuner(ActionResolver _actionResolver) {
+        mActionResolver = _actionResolver;
+    }
 
-	/**
-	 * Loads any assets, creates a sprite batch used throughout the application and sets an instance
+    /**
+     * Loads any assets, creates a sprite batch used throughout the application and sets an instance
      * of {@link com.fraserjohnstone.tuner.screens.SplashScreen} as the current screen
-	 */
-	@Override
-	public void create () {
-		//get screen dimensions
-		mScreenHeightPixels = Gdx.graphics.getHeight();
-		mScreenWidthPixels = Gdx.graphics.getWidth();
+     */
+    @Override
+    public void create() {
+        //get screen dimensions
+        mScreenHeightPixels = Gdx.graphics.getHeight();
+        mScreenWidthPixels = Gdx.graphics.getWidth();
 
         //load assets
-		loadAssets();
+        loadAssets();
 
-		//create SpriteBatch
-		mBatch = new SpriteBatch();
+        //create SpriteBatch
+        mBatch = new SpriteBatch();
 
         //set screen to splash screen
-		setScreen(new com.fraserjohnstone.tuner.screens.SplashScreen(this));
-	}
+        setScreen(new com.fraserjohnstone.tuner.screens.SplashScreen(this));
+    }
 
-	/**
-	 * Loads all of the assets into {@link #mAssetManager}.
-	 */
-	private void loadAssets(){
-		//create the parameters for image assets
-		TextureParameter param = new TextureParameter();
-		param.genMipMaps = true;
-		param.minFilter = TextureFilter.MipMapLinearNearest;
-		param.magFilter = TextureFilter.Nearest;
+    /**
+     * Loads all of the assets into {@link #mAssetManager}.
+     */
+    private void loadAssets() {
+        //create the parameters for image assets
+        TextureParameter param = new TextureParameter();
+        param.genMipMaps = true;
+        param.minFilter = TextureFilter.MipMapLinearNearest;
+        param.magFilter = TextureFilter.Nearest;
 
         //load all images into the AssetManager
-		mAssetManager = new AssetManager();
+        mAssetManager = new AssetManager();
 
         //images
-		for(int i = 0; i < com.fraserjohnstone.tuner.assets.LoadableAssets.assetArray.length; i++){
-			mAssetManager.load(com.fraserjohnstone.tuner.assets.LoadableAssets.assetArray[i], Texture.class, param);
-		}
+        for (int i = 0; i < com.fraserjohnstone.tuner.assets.LoadableAssets.assetArray.length; i++) {
+            mAssetManager.load(com.fraserjohnstone.tuner.assets.LoadableAssets.assetArray[i], Texture.class, param);
+        }
 
         //bitmap fonts
-        for(int i = 0; i< com.fraserjohnstone.tuner.assets.LoadableAssets.bitmapFontAssetArray.length; i++){
+        for (int i = 0; i < com.fraserjohnstone.tuner.assets.LoadableAssets.bitmapFontAssetArray.length; i++) {
             mAssetManager.load(com.fraserjohnstone.tuner.assets.LoadableAssets.bitmapFontAssetArray[i], BitmapFont.class);
         }
-	}
+    }
 
-	@Override
-	public void render(){
-		super.render();
-	}
+    @Override
+    public void render() {
+        super.render();
+    }
 
     /**
      * This method safely disposes of any disposable objects (see {@link com.badlogic.gdx.utils.Disposable})
      * created in this class.
      */
-	@Override
-	public void dispose () {
-		mBatch.dispose();
-		if(mAssetManager != null){
-			mAssetManager.dispose();
-		}
-	}
+    @Override
+    public void dispose() {
+        mBatch.dispose();
+        if (mAssetManager != null) {
+            mAssetManager.dispose();
+        }
+    }
 }
